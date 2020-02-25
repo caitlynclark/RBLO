fcd024a7c1047e61845798ae4617da3ccf474554
Caitlyn E. Clark, PhD <caitlyn.clark@nrel.gov>
2020-02-25 11:35:48 -0700

    Update drivetrain.py
    Added comments and validated L10 calculation

--- drivetrain.py
+++ drivetrain.py
@@ -5,7 +5,6 @@ Created on Mon Dec 30 10:10:11 2019
 @author: cclark2
 """
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,7 +18,7 @@ class Drivetrain():
         
         '''Instantiate LayoutOptimization object and parameter values.'''
 
-        self.FF_timestep = FF_timestep
+        self.FF_timestep = FF_timestep      # FAST.Farm timestep for outputs
         self.m_c = m_c                      # carrier mass
         self.d_c = d_c                      # center distance 
         self.m_s = m_s                      # shaft mass
@@ -31,33 +30,33 @@ class Drivetrain():
         self.L_s = L_s                      # distance from main bearing to the main shaft's center of gravity
         self.L_p = L_p                      # distance from main bearing to the planet bearing's center of gravity
         self.rho = rho                      # bedplate tilting angle (if don't want to include, set to 0 degrees)
-        self.C = C                          # capacity from bearing, N
+        self.C = C                          # bearing basic dynamic load rating or capacity, N (the load that a bearing can carry for 1 million inner-race revolutions with a 90% probability of survival)
         self.e = e                          # constant for roller bearings
         
 
-    def pl(self, R, torque, m_y, m_z):
+    def pl(self, R, alpha, torque, m_y, m_z):
     
-        '''Calculation of forces via Eq 9 in Guo et al. 2015, Wind Energy'''
+        '''Calculation of forces via Eq 9 in Guo et al. 2015, Wind Energy. Inconsistencies between this code and the equations in the paper have been confirmed with Guo in January 2020'''
 
         omega = 2 * np.pi/self.N            # angle from planetary gear center to bearing center
         temp = np.zeros((2 * self.N,))      # sun planet mesh loads
 
-        for i in range(self.N): 
-            temp[0] = temp[0] - R[i] * np.cos(omega * i + self.beta[i]) + (self.m_p * self.g * np.sin(omega * i + self.beta[i]) ** 2) * np.cos(self.rho)
+        for i in range(self.N): # parts of Eq. 9 that require summation over the bearings
+            temp[0] = temp[0] - R[i] * np.cos(omega * i + alpha + self.beta[i]) + (self.m_p * self.g * np.sin(omega * i + alpha + self.beta[i]) ** 2) * np.cos(self.rho)
             temp[1] = temp[1] + R[i]
-            temp[2] = temp[2] + R[i] * np.sin(omega * i + self.beta[i]) + self.m_c * self.g * np.sin(omega * i + self.beta[i]) * np.cos(omega * i + self.beta[i]) * np.cos(self.rho)
+            temp[2] = temp[2] + R[i] * np.sin(omega * i + alpha + self.beta[i]) + self.m_c * self.g * np.sin(omega * i + alpha + self.beta[i]) * np.cos(omega * i + alpha + self.beta[i]) * np.cos(self.rho)
 
         z = np.zeros((self.N,))
         z[0] = temp[0] + (self.m_c * self.g * np.cos(self.rho)) + ((0.5 * self.m_s * self.g * self.L_s * np.cos(self.rho)) / self.L_c) + (m_y / self.L_c)        
         z[1] = temp[1] * self.d_c - torque 
-        z[2] = -temp[2] * self.L_p - m_z #z not included, but could, here (currently yaw moment is equal to 0) 
+        z[2] = -temp[2] * self.L_p - m_z  
 
-        return z #this value makes up the Jacobian
+        return z #this value makes up the Jacobian       
+    
 
-
-    def fsolve_TE3(self, fun, x0, tol, max_fsolve_iter, torque, m_y, m_z):
+    def fsolve_TE3(self, fun, x0, tol, max_fsolve_iter, alpha, torque, m_y, m_z):
         
-        '''Solver function. Can be replaced by other solvers.'''
+        '''Function that solves for the Jacobian matrix. Can be replaced by other built-in solvers.'''
         
         fsolve_iter = 0 
         error = 10 
@@ -80,10 +79,10 @@ class Drivetrain():
                 xplus[i]  = E[i][1] 
                 xminus[i] = E[i][0] 
 
-                J = (self.pl(xplus, torque, m_y, m_z) - self.pl(xminus, torque, m_y, m_z)) / H[i] 
+                J = (self.pl(xplus, alpha, torque, m_y, m_z) - self.pl(xminus, alpha, torque, m_y, m_z)) / H[i] 
                 Jacobian[:,i] = J 
                 
-            f = self.pl(x0, torque, m_y, m_z).conj().T 
+            f = self.pl(x0, alpha, torque, m_y, m_z).conj().T 
             error = np.sqrt(np.sum(f ** 2)) 
             inv_result = np.linalg.solve(Jacobian, f)
             x1 = x0 - inv_result
@@ -102,58 +101,37 @@ class Drivetrain():
         return x1 #planet forces
 
 
-    def calc_planet_forces(self, planet_speed, torque, m_y, m_z):
+    def calc_planet_forces(self, planet_speed, alpha, torque, m_y, m_z):
         
-        '''Calculate planet bearing forces'''
+        '''Calculate planet bearing forces: calls the fsolve_TE3 solver which builds and solves the Jacobian matrix made of planetary forces calculated in the pl function'''
 
         planet_forces = np.zeros((len(torque), self.N)) 
         R0 = np.asarray([1E3,1E3,1E3])    #initial guess for ring planet mesh forces
 
         for j in range(len(torque)):
-            planet_forces[j,:] = self.fsolve_TE3(self.pl, R0, 0.01, 200, torque[j], m_y[j], m_z[j]) # planet forces (tangential) (requires inputs/outputs in N-m)
+            planet_forces[j,:] = self.fsolve_TE3(self.pl, R0, 0.01, 200, alpha[j], torque[j], m_y[j], m_z[j]) # planet forces (tangential) (requires inputs/outputs in N-m)
             R0 = planet_forces[j,:] #updates initial guess for faster computation
-        return planet_forces[:, 0], planet_speed
+        return planet_forces[:, 0], planet_speed #only return one bearing's forces, if you are assuming even load distribution 
 
 
     def calc_L10(self, planet_forces, planet_speed): 
         
-        '''Calculate L10 for the planet bearings given the planet bearing force and speed time history'''
-        L10 = []
-        T = self.FF_timestep / (len(planet_speed) * self.FF_timestep - self.FF_timestep) # contribution
-        L10 = [float(1/(T/((10**6)/(60*j)*(self.C/abs(i))**self.e))) for i,j in zip(planet_forces, planet_speed)] #in hours
+        '''Calculates L10 for the planet bearings given the planet bearing force and speed time history. L10 life is the time that 90% of a group of bearings will exceed without failing by rolling-element fatigue.'''
 
-#        L10 = sum(L10)
+        T = self.FF_timestep / (len(planet_speed) * self.FF_timestep - self.FF_timestep) # fraction of total running time at a given load and speed
+        L10 = [T/((10**6/(60*i))*(self.C/abs(j))**self.e) for i,j in zip(planet_speed, planet_forces)] # life at each load and speed combination, in hours
+        # 10**6 million race revolutions conversion factor
+        # 60 min/hr conversion factor
+        # i: planet speed 
+        # C: bearing basic dynamic load rating or capacity, N (the load that a bearing can carry for 1 million inner-race revolutions with a 90% probability of survival)
+        # e: load-life exponent (determined by Lundberg and Palmgren to be 3 for ball bearings and 10/3 for cylindrical roller bearings)
+        L10_total = 1/sum(L10) # total life in hours over varying loads and speeds, Eq. 14 from "Rolling Bearing Life Prediction, Theory, and Application," by Zaretsky (2016)
                      
-        return L10
-    
-
-    def plot_timeseries(self, x, ylim_min, ylim_max, xlabel, ylabel, filename):    
-        '''Plot timeseries of torque, planet forces, or planet speed'''
-
-        plt.scatter(range(len(x)), x, s = 1)
-        # plt.title(str(title))
-        plt.ylim(ylim_min, ylim_max)
-        plt.xlabel(str(xlabel))
-        plt.ylabel(str(ylabel))
-        plt.savefig(str(filename))
-        plt.show()
-
-
-    def plot_scatter(self, x, y, xlim_min, xlim_max, ylim_min, ylim_max, xlabel, ylabel, filename):    
-        '''Plot timeseries of torque, planet forces, or planet speed'''
-
-        plt.scatter(x, y, s = 1)
-        # plt.title(str(title))
-        plt.xlabel(str(xlabel))
-        plt.xlim(xlim_min, xlim_max)
-        plt.ylim(ylim_min, ylim_max)
-        plt.xticks(rotation=90)
-        plt.ylabel(str(ylabel))
-        plt.savefig(str(filename))
-        plt.show()
-
+        return L10, L10_total #returns L10, or the vector of life calculations at each point in the time series
+  
 
     def plot_loads(self, x1, x2, x3, x4, x1_label, x2_label, x3_label, x4_label, xlabel, ylabel, filename):
+        
         '''Plot torque and non-torque loads'''
 
         plt.plot(range(len(x1)), x1, alpha=0.5, label = str(x1_label)) 
