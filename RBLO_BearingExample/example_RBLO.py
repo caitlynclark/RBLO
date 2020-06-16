8820cdd94227d8de8f13ad4f9a73c75484e4ccde
Caitlyn E. Clark, PhD <caitlyn.clark@nrel.gov>
2020-06-16 12:06:53 -0600

    Update example_rblo.py after review

--- RBLO_BearingExample/example_RBLO.py
+++ RBLO_BearingExample/example_RBLO.py
@@ -8,26 +8,39 @@ Created on Mon Oct  7 14:09:54 2019
 
 #Internal Modules
 import random
+from itertools import combinations
 
 #External Modules
 import floris.tools.rblo as opt
 
 
-seeds = 1                           # for multi-starts
+'''Parameters to test
+#wd = [0, 90, 135, 180]
+#ws = [8, 12, 18]
+#shear = [0.1, 0.2, 0.3]
+#TI = [5, 10, 15, 20]
+'''
+
+seeds = 2                           # for multi-starts
 
 # Define wind rose data
-wd = [270, 315, 0]                  # wind direction vector
-ws = [8]                            # wind speed (make sure it aligns with interpolant surfaces)
-freq = [(1/3), (1/3), (1/3)]        # frequency of wind direction
+wd = [0]                            # wind direction 
+wd_freq = [1]                       # wind direction frequency
+
+ws = [8]                            # wind speed
+ws_freq = [1]                       # wind speed frequency
+
+shear = 0.1 
+TI = 10 
 
 # Set turbine and array parameters 
 D = 126                             # diameter of rotor (m)
-N = 10                              # number of turbines
+N = 5                               # number of turbines
 
 min_dist = 5*D                      # minimum separation distance
 
 x_min = 0
-x_max = 1 #design variables are set between 0 and 2*D*N, farm boundaries
+x_max = 1                           
 y_min = 0
 y_max = 1
 
@@ -37,40 +50,78 @@ bndx_max = 1890
 bndy_min = 0
 bndy_max = 1890
 
-for jj in range(seeds):
-        
-    # Random layouts    
-    layout_x = [random.uniform(0, x_max) for ii in range(N)] 
-    layout_y = [random.uniform(0, y_max) for ii in range(N)] 
-    
-    # Set optimization options
-    opt_options = {'maxiter': 400, 'disp': True, 'iprint': 2, 'ftol': 1e-8}
-    
-    # Instantiate the layout optimization object
-    layout_opt = opt.RBLO(
-                            N = N, 
-                            D = D, 
-                            layout_x = layout_x, 
-                            layout_y = layout_y, 
-                            x_min = x_min, 
-                            x_max = x_max, 
-                            y_min = y_min, 
-                            y_max = y_max,
-                            bndx_min = bndx_min,
-                            bndy_min = bndy_min,
-                            bndx_max = bndx_max,                        
-                            bndy_max = bndy_max,
-                            wd=wd,
-                            ws=ws,
-                            freq=freq,
-                            min_dist = min_dist,
-                            cons = None,
-                            opt_options=opt_options,
-    )
-        
-    # Run optimization
-    layout_results = layout_opt.optimize()
-    print('Layout Results: ', layout_results)
+average = []
 
-    # Plot the new layout vs the old layout
-    layout_opt.plot_layout_opt_results(jj)
+#for ii in range(2, 16):
+
+#    N = ii
+objective = []
+cost = []
+power = []
+replacements = []
+
+count = 0
+
+for jj in range(seeds):
+
+    while count <= seeds:        
+    #    # Random layouts    
+        layout_x = [random.uniform(0, x_max) for ii in range(N)] 
+        layout_y = [random.uniform(0, y_max) for ii in range(N)] 
+            
+        # Set optimization options
+        opt_options = {'maxiter': 600, 'disp': True, 'iprint': 2, 'ftol': 1e-8}
+        
+        # Instantiate the layout optimization object
+        layout_opt = opt.RBLO(  N = N, 
+                                D = D, 
+                                layout_x = layout_x, 
+                                layout_y = layout_y, 
+                                x_min = x_min, 
+                                x_max = x_max, 
+                                y_min = y_min, 
+                                y_max = y_max,
+                                bndx_min = bndx_min,
+                                bndy_min = bndy_min,
+                                bndx_max = bndx_max,                        
+                                bndy_max = bndy_max,
+                                wd = wd,
+                                wd_freq = wd_freq,
+                                ws = ws,
+                                ws_freq = ws_freq,
+                                TI = TI,
+                                shear = shear,
+                                min_dist = min_dist,
+                                cons = None,
+                                opt_options=opt_options,
+        )
+                
+        
+        # Run optimization
+        opt_locs = layout_opt.optimize()
+        locsx_D = [x/D for x in opt_locs[0]]
+        locsy_D = [x/D for x in opt_locs[1]]
+    
+        
+        loc_combos = list(combinations(zip(locsx_D, locsy_D), 2))
+        
+        distance = [layout_opt._calc_distance(ii[0][0], ii[0][1], ii[1][0], ii[1][1]) for ii in loc_combos]
+        if all(ii >= 5 for ii in distance): 
+                    
+            # Plot the new layout vs the old layout
+            objective_temp, cost_temp, power_temp, replacements_temp = layout_opt.plot_layout_opt_results(count)
+            objective.append(objective_temp)
+            cost.append(cost_temp)
+            power.append(power_temp)
+            replacements.append(replacements_temp)
+            count += 1
+    
+        else:
+            print('Constraint violated')
+            
+    
+print('Objective: ', objective)
+print('Cost: ', cost)
+print('Power: ', power)
+print('Replacements: ', replacements)
+print('Wind Direction: ', wd, 'No. Turbines: ', N, 'TI: ', TI, 'Shear: ', shear, 'Velocity: ', ws)
